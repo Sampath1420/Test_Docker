14810ba2cd680f7df106dc849e7c4cb0490d6d78
Sampath1420 <kayaguralasampathkumar@gmail.com>
2025-11-08 11:05:57 +0530

    Delete sample directory

--- sample/etl/load.py
+++
@@ -1,8 +0,0 @@
-from typing import List, Dict
-from .utils.db import ensure_table, upsert_rows
-
-def load_dataframe(df) -> int:
-    ensure_table()
-    rows: List[Dict] = df.to_dict(orient="records")
-    affected = upsert_rows(rows)
-    return affected
